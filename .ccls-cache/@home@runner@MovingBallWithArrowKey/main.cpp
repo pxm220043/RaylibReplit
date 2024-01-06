affd69819070e7f1353537b266e66050e02e6a80
pxm220043 <145625808+pxm220043@users.noreply.github.com>
2024-01-06 05:54:55 +0000

    Pong_Game_C++_Raylib

--- .ccls-cache/@home@runner@MovingBallWithArrowKey/main.cpp
+++ .ccls-cache/@home@runner@MovingBallWithArrowKey/main.cpp
@@ -1,51 +1,164 @@
 #include "raylib.h"
+#include <cmath> 
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 500;
-    const int screenHeight = 500;
+int player_score = 0;
+int cpu_score = 0;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+Color Green = {38, 185, 154, 255};
+Color Dark_Green = {20, 160, 133, 255};
+Color Light_Green = {129, 204, 184, 255};
+Color Yellow = {243, 213, 91, 255};
 
-    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+class Ball {
+public:
+  float x, y;
+  int speed_x, speed_y;
+  int radius;
+  void Draw() { DrawCircle(x, y, radius, Yellow); }
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+  void Update() {
+    x += speed_x;
+    y += speed_y;
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    if (y + radius >= GetScreenHeight() || y - radius <= 0) {
+      speed_y *= -1;
+    }
+    if (x + radius >= GetScreenWidth()) // cpu wins
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_D)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_A)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_W)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_S)) ballPosition.y += 2.0f;
-        //----------------------------------------------------------------------------------
+      cpu_score++;
+      ResetBall();
+    }
+    if (x - radius <= 0) {
+      player_score++;
+      ResetBall();
+    }
+  }
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+void ResetBall() {
+    x = GetScreenWidth() / 2;
+    y = GetScreenHeight() / 2;
 
-            ClearBackground(RAYWHITE);
+    int speed_choices[2] = {1, -1};
+    speed_x = speed_choices[GetRandomValue(0, 1)] * std::abs(speed_x);
+    speed_y = speed_choices[GetRandomValue(0, 1)] * std::abs(speed_y);
+}
+};
 
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+class Paddle {
 
-            DrawCircleV(ballPosition, 50, MAROON);
+protected:
+  void LimitMovement() {
+    if (y + height >= GetScreenHeight()) {
+      y = GetScreenHeight() - height;
+    }
+    if (y <= 0) {
+      y = 0;
+    }
+  };
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+public:
+  float x, y;
+  float width, height;
+  int speed;
+
+  void Draw() {
+    DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
+  }
+
+  void Update() {
+    if (IsKeyDown(KEY_W)) {
+      y -= speed;
+    }
+    if (IsKeyDown(KEY_S)) {
+      y += speed;
+    }
+    if (y + height >= GetScreenHeight()) {
+      y = GetScreenHeight() - height;
+    }
+    if (y <= 0) {
+      y = 0;
+    }
+  }
+}; // Added the missing semicolon here
+
+class CpuPaddle : public Paddle {
+public:
+  void Update(int ball_y) {
+    if (y + height / 2 > ball_y) {
+      y = y - speed;
+    }
+    if (y + height / 2 <= ball_y) {
+      y = y + speed;
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    LimitMovement();
+  }
+};
 
-    return 0;
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
+
+int main() {
+  const int screenWidth = 500;
+  const int screenHeight = 500;
+  InitWindow(screenWidth, screenHeight, "My pong game!");
+  SetTargetFPS(60);
+
+  ball.radius = 20;
+  ball.x = screenWidth / 2;
+  ball.y = screenHeight / 2;
+  ball.speed_x = 7;
+  ball.speed_y = 7;
+
+  player.width = 25;
+  player.height = 100;
+  player.x = screenWidth - player.width - 10;
+  player.y = screenHeight / 2 - player.height / 2;
+  player.speed = 6;
+
+  cpu.height = 120;
+  cpu.width = 25;
+  cpu.x = 10;
+  cpu.y = screenHeight / 2 - cpu.height / 2;
+  cpu.speed = 6;
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+
+    ball.Update();
+    player.Update();
+    cpu.Update(ball.y);
+
+    if (CheckCollisionCircleRec(
+            Vector2{ball.x, ball.y}, ball.radius,
+            Rectangle{player.x, player.y, player.width, player.height})) {
+      ball.speed_x *= -1;
+    }
+
+    if (CheckCollisionCircleRec(
+            Vector2{ball.x, ball.y}, ball.radius,
+            Rectangle{cpu.x, cpu.y, cpu.width, cpu.height})) {
+      ball.speed_x *= -1;
+    }
+
+    ClearBackground(Dark_Green);
+    DrawRectangle(screenWidth / 2, 0, screenHeight / 2, screenHeight, Green);
+
+    DrawCircle(screenWidth / 2, screenHeight / 2, 150, Light_Green);
+
+    // Draw game elements here
+    DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+    ball.Draw();
+    cpu.Draw();
+    player.Draw();
+    DrawText(TextFormat("%i", cpu_score), screenWidth / 4 - 20, 20, 80, WHITE);
+    DrawText(TextFormat("%i", player_score), 3 * screenWidth / 4 - 20, 20, 80,
+             WHITE);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }
